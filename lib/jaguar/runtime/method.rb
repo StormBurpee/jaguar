7aafbac4472fafb7ac73fbbebee5944b122ca7a1
Storm Burpee <stormburpee@gmail.com>
2018-07-30 16:02:08 +0930

    basic methods added to runtime

---
+++ lib/jaguar/runtime/method.rb
@@ -0,0 +1,20 @@
+module Jaguar
+
+  class JaguarMethod
+    def initialize(params, body)
+      @params = params
+      @body = body
+    end
+
+    def call(receiver, arguments)
+      context = Context.new(receiver)
+
+      @params.each_with_index do |param, index|
+        context.locals[param] = arguments[index]
+      end
+
+      @body.eval(context)
+    end
+  end
+
+end
