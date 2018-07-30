d5df7918499fafeb8c4b1dc70730dd5153fccd6d
Storm Burpee <stormburpee@gmail.com>
2018-07-30 19:14:04 +0930

    adding == && != operators

--- lib/jaguar/runtime/bootstrap.rb
+++ lib/jaguar/runtime/bootstrap.rb
@@ -9,9 +9,10 @@ module Jaguar
 
   Runtime["Class"] = jaguar_class
   Runtime["Object"] = object_class
-  Runtime["Number"] = JaguarClass.new
-  Runtime["Bool"] = JaguarClass.new
-  Runtime["String"] = JaguarClass.new
+
+  Runtime["Number"] = JaguarClass.new(Runtime["Object"])
+  Runtime["Bool"] = JaguarClass.new(Runtime["Object"])
+  Runtime["String"] = JaguarClass.new(Runtime["Object"])
 
   Runtime["true"] = Runtime["Bool"].new_with_value(true)
   Runtime["false"] = Runtime["Bool"].new_with_value(false)
@@ -20,16 +21,22 @@ module Jaguar
   Runtime["Class"].runtime_methods["new"] = proc do |receiver, arguments|
     receiver.new
   end
-  Runtime["Class"].runtime_methods["to_string"] = proc do |receiver, arguments|
-    puts receiver
-    puts arguments
-    Runtime["String"].new_with_value("")
-  end
 
   # Object Initializations
   Runtime["Object"].runtime_methods["print"] = proc do |receiver, arguments|
     puts arguments.first.ruby_value
     Runtime["null"]
+  end
+  Runtime["Object"].runtime_methods["to_string"] = proc do |receiver, arguments|
+    puts receiver
+    puts arguments
+    Runtime["String"].new_with_value("")
+  end
+  Runtime["Object"].runtime_methods["=="] = proc do |receiver, arguments|
+    Runtime["Bool"].new_with_value(receiver.ruby_value == arguments.first.ruby_value)
+  end
+  Runtime["Object"].runtime_methods["!="] = proc do |receiver, arguments|
+    Runtime["Bool"].new_with_value(receiver.ruby_value != arguments.first.ruby_value)
   end
 
   #Number Initializations
@@ -50,9 +57,6 @@ module Jaguar
   end
   Runtime["Number"].runtime_methods["--"] = proc do |receiver, arguments|
     Runtime["Number"].new_with_value(receiver.ruby_value - 1)
-  end
-  Runtime["Number"].runtime_methods["=="] = proc do |receiver, arguments|
-    Runtime["Bool"].new_with_value(receiver.ruby_value == arguments.first.ruby_value)
   end
 
 end
