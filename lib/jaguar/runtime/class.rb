a06879833c3fa80ab7df9e380a42ca03048db759
Storm Burpee <stormburpee@gmail.com>
2018-07-30 15:59:11 +0930

    Class runtime added, everything will be a class (which is an object)

---
+++ lib/jaguar/runtime/class.rb
@@ -0,0 +1,35 @@
+module Jaguar
+
+  class JaguarClass < JaguarObject
+    attr_reader :runtime_methods
+
+    def initialize
+      @runtime_methods = {}
+
+      if defined? Runtime
+        runtime_class = Runtime['Class']
+      else
+        runtime_class = nil
+      end
+
+      super(runtime_class)
+    end
+
+    def lookup(method_name)
+      method = @runtime_methods[method_name]
+      unless method
+        raise "Method not found #{method_name}"
+      end
+      method
+    end
+
+    def new
+      JaguarClass.new(self)
+    end
+
+    def new_with_value(value)
+      JaguarClass.new(self, value)
+    end
+  end
+
+end
