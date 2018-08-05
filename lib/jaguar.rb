0f7bbc2fbaeb3c228af0625e4c5cb88a97948802
Storm Burpee <stormburpee@gmail.com>
2018-08-05 14:58:43 +0930

    cleaning up preparing for rewrite

--- lib/jaguar.rb
+++
@@ -1,9 +0,0 @@
-require_relative "jaguar/version"
-require_relative "jaguar/lexer"
-require_relative "jaguar/parser"
-require_relative "jaguar/nodes"
-require_relative "jaguar/runtime"
-
-module Jaguar
-
-end
