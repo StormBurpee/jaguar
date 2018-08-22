bbc67a7ca24dd0b84480a51b00f884eeac7ae89b
Storm Burpee <stormburpee@gmail.com>
2018-08-23 01:18:25 +0930

    finishing up Container, decoupled from laravel

---
+++ src/Jaguar/Container/EntryNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jaguar\Container;
+
+use Exception;
+use Psr\Container\NotFoundExceptionInterface;
+
+class EntryNotFoundException extends Exception implements NotFoundExceptionInterface
+{
+    //
+}
