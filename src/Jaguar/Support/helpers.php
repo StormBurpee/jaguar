0358a3d9869f3bbf77ead14faf06a22ff56ad797
Storm Burpee <stormburpee@gmail.com>
2018-08-23 00:36:20 +0930

    expanding support helpers

--- src/Jaguar/Support/helpers.php
+++ src/Jaguar/Support/helpers.php
@@ -3,6 +3,7 @@ use Jaguar\Support\Arr;
 use Jaguar\Support\Str;
 use Jaguar\Support\Optional;
 use Jaguar\Support\Debug\Dumper;
+use Jaguar\Contracts\Support\Htmlable;
 use Jaguar\Support\HigherOrderTapProxy;
 
 if(! function_exists('e')) {
@@ -47,5 +48,54 @@ if (! function_exists('tap')) {
 
         $callback($value);
         return $value;
+    }
+}
+
+if (! function_exists('transform')) {
+    /**
+     * Transform the given value if it is present.
+     *
+     * @param  mixed  $value
+     * @param  callable  $callback
+     * @param  mixed  $default
+     * @return mixed|null
+     */
+    function transform($value, callable $callback, $default = null)
+    {
+        if (filled($value)) {
+            return $callback($value);
+        }
+
+        if (is_callable($default)) {
+            return $default($value);
+        }
+
+        return $default;
+    }
+}
+
+if (! function_exists('windows_os')) {
+    /**
+     * Determine whether the current environment is Windows based.
+     *
+     * @return bool
+     */
+    function windows_os()
+    {
+        return strtolower(substr(PHP_OS, 0, 3)) === 'win';
+    }
+}
+
+if (! function_exists('with')) {
+    /**
+     * Return the given value, optionally passed through the given callback.
+     *
+     * @param  mixed  $value
+     * @param  callable|null  $callback
+     * @return mixed
+     */
+    function with($value, callable $callback = null)
+    {
+        return is_null($callback) ? $value : $callback($value);
     }
 }
