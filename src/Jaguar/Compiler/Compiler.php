26fd691057797bb205d3a943303853d241e6f711
Storm Burpee <stormburpee@gmail.com>
2018-08-23 17:04:13 +0930

    further work on standalone compiler

--- src/Jaguar/Compiler/Compiler.php
+++ src/Jaguar/Compiler/Compiler.php
@@ -3,48 +3,30 @@
 namespace Jaguar\Compiler;
 
 use InvalidArgumentException;
-use Jaguar\Foundation\Filesystem\Filesystem;
+use Jaguar\Support\Filesystem\Filesystem;
 
 abstract class Compiler
 {
-    /**
-    * The filesystem instance
-    *
-    * @var \Jaguar\Foundation\Filesystem\Filesystem
-    */
-    protected $files;
+  /**
+   * The filesystem instance
+   * @var \Jaguar\Support\Filesystem\Filesystem
+   */
+  protected $files;
 
-    /**
-    * Cache path for the compiled views.
-    *
-    * @var string
-    */
-    protected $cachePath;
+  /**
+   * The path for the compiled view.
+   * @var string
+   */
+  protected $compilePath;
 
-    public function __construct(Filesystem $files, $cachePath)
-    {
-        if (! $cachePath) {
-            throw new InvalidArgumentException("Please provide a valid cache path.");
-        }
-
-        $this->files = $files;
-        $this->cachePath = $cachePath;
-    }
-
-    public function getCompiledPath($path)
-    {
-        return $this->cachePath.'/'.sha1($path).'.php';
-    }
-
-    public function isExpired($path)
-    {
-        $compiled = $this->getCompiledPath($path);
-
-        if (! $this->files->exists($compiled)) {
-            return true;
-        }
-
-        return $this->files->lastModified($path) >=
-           $this->files->lastModified($compiled);
-    }
+  /**
+   * Create a new compiler instance.
+   * @param \Jaguar\Support\Filesystem\Filesystem $files
+   * @param string     $compilePath
+   */
+  public function __construct(Filesystem $files, $compilePath)
+  {
+    $this->files = $files;
+    $this->compilePath = $compilePath;
+  }
 }
---
+++ src/Jaguar/Contracts/Support/Filesystem/FileNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jaguar\Contracts\Support\Filesystem;
+
+use Exception;
+
+class FileNotFoundException extends Exception
+{
+  //
+}
